63fa7916fda54deb86b916e5adc9bcb0fd60143f
Jacob Bachmann <jacob.bachmann@posteo.de>
2023-09-19 18:16:25 +0200

    chore: remove comment

--- users/gandalf/modules/zsh.nix
+++ users/gandalf/modules/zsh.nix
@@ -1,22 +1,10 @@
-{ config, pkgs, ... }: {
+{ config, ... }: {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     enableAutosuggestions = true;
     syntaxHighlighting.enable = true;
-    /*
-    plugins = [
-      {
-        name = "zsh-syntax-highlighting";
-        src = pkgs.fetchFromGitHub {
-          owner = "zsh-users";
-          repo = "zsh-syntax-highlighting";
-          rev = "0.7.1";
-          hash = "sha256-gOG0NLlaJfotJfs+SUhGgLTNOnGLjoqnUp54V9aFJg8=";
-        };
-      }
-    ];
-    */
+
     oh-my-zsh = {
       enable = true;
       theme = "terminalparty";
