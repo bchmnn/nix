7692303804c71fbb2191f6c0947315f39c6a8c9e
Jacob Bachmann <jacob.bachmann@posteo.de>
2024-01-08 17:22:24 +0100

    feat: add printer driver

--- modules/nix.nix
+++ modules/nix.nix
@@ -3,5 +3,13 @@
     experimental-features = [ "nix-command" "flakes" ];
     auto-optimise-store = true;
   };
+
+  nixpkgs.overlays = [
+    (self: super: {
+      mfcl3750cdwlpr = (super.callPackage ../overlays/mfcl3750cdw.nix {}).driver;
+      mfcl3750cdwcupswrapper = (super.callPackage ../overlays/mfcl3750cdw.nix {}).cupswrapper;
+    })
+  ];
+
   system.stateVersion = "23.05";
 }
--- modules/printing.nix
+++ modules/printing.nix
@@ -1,6 +1,11 @@
-{ ... }: {
+{ pkgs, config, ... }: {
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+  services.printing = {
+    enable = true;
+    drivers = [
+      pkgs.mfcl3750cdwcupswrapper
+    ];
+  };
 
   services.avahi = {
     enable = true;
@@ -8,4 +13,20 @@
     openFirewall = true;
   };
 
+  /*
+  hardware.printers = {
+    ensurePrinters = [
+      {
+        name = "Brother MFC-L3750CDW series";
+        location = "Home";
+        deviceUri = "ipp://BROTHER-ETH.local.:631/ipp/print";
+        model =
+      }
+    ]
+  }
+  */
+
+  environment.systemPackages = with pkgs; with config.bchmnn; lib.optionals (gui.enable) [
+    system-config-printer
+  ];
 }
