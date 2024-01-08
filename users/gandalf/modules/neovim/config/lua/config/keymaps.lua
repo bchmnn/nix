edbc3bdac4ae62aa33bd59e4b4256df0a110294f
Jacob Bachmann <jacob.bachmann@posteo.de>
2024-01-08 17:51:55 +0100

    config(nvim): unmap navigation keymaps in terminal mode

--- users/gandalf/modules/neovim/config/lua/config/keymaps.lua
+++ users/gandalf/modules/neovim/config/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.map
+local unmap = vim.keymap.del
+
+unmap("t", "<C-h>")
+unmap("t", "<C-j>")
+unmap("t", "<C-k>")
+unmap("t", "<C-l>")
