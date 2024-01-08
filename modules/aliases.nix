d3bc83fbd601d68e53b3429cc9a7a4edc32cc1e9
Jacob Bachmann <jacob.bachmann@posteo.de>
2024-01-08 18:00:09 +0100

    feat: add docker aliases

--- modules/aliases.nix
+++ modules/aliases.nix
@@ -3,6 +3,7 @@
   la = "lsd -lAh --group-directories-first";
   lt = "lsd -lAh --group-directories-first --tree";
   llt = "unbuffer lsd -lAh --group-directories-first --tree | less -r";
+
   gg = "cd ~/git";
   gs = "git status";
   gl = "git log --oneline";
@@ -10,11 +11,20 @@
   gaa = "git add --all";
   gc = "git commit -m";
   gp = "git push";
+
   ta = "tmux attach";
+
   nec = "nvim --cmd \"cd $(readlink -f /etc/nixos)\"";
   ncc = "sudo nixos-rebuild switch --upgrade-all --flake \"$(readlink -f /etc/nixos)\"";
   nup = "sudo nix-channel --update";
   nuf = "sudo nix flake update --nix-path /etc/nixos";
   ngc = "sudo nix-collect-garbage -d";
+
+  dcup = "docker-compose up --detach --remove-orphans";
+  dka = "docker kill $(docker ps -q)";
+  drmc = "docker container rm $(docker container ls -aq)";
+  drmi = "docker image rm $(docker image ls -aq)";
+  drmv = "docker volume rm $(docker volume ls -q)";
+
   lsip = "curl -s 'https://nordvpn.com/wp-admin/admin-ajax.php?action=get_user_info_data' | jq";
 }
