9b09fedcbfe694bccfe2f13a91c0851a738822aa
Jacob Bachmann <jacob.bachmann@posteo.de>
2024-01-08 17:51:12 +0100

    config(nvim): add syntax highlighting for .y files

--- users/gandalf/modules/neovim/config/lua/config/options.lua
+++ users/gandalf/modules/neovim/config/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Set the filetype for .y files to be the same as .lex files
+vim.api.nvim_exec([[ au BufRead,BufNewFile *.y set filetype=lex ]], false)
