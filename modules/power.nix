e88e31bb118c1cac568fe7301abc92b17a6f35bf
Jacob Bachmann <jacob.bachmann@posteo.de>
2024-01-16 20:14:49 +0100

    feat: replace powertop with tlp

--- modules/power.nix
+++ modules/power.nix
@@ -1,6 +1,14 @@
-{ ... }: {
+{ pkgs, ... }: {
   powerManagement = {
     enable = true;
-    powertop.enable = true;
+    # powertop.enable = true;
   };
+
+  services.tlp = {
+    enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    powertop
+  ];
 }
