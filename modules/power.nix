509a60d63b388ad05dd9f3aeccca00f34fcaa24e
Jacob Bachmann <jacob.bachmann@posteo.de>
2024-01-21 01:03:44 +0100

    feat: adjust performance profile settings

--- modules/power.nix
+++ modules/power.nix
@@ -6,6 +6,12 @@
 
   services.tlp = {
     enable = true;
+    settings = {
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "balanced";
+    };
   };
 
   environment.systemPackages = with pkgs; [
