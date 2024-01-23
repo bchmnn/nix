b25967225e589466c642afd2103bb2baab9dad4a
Jacob Bachmann <jacob.bachmann@posteo.de>
2024-01-23 01:18:50 +0100

    feat: added disk analyzer utilities

--- users/gandalf/modules/packages.nix
+++ users/gandalf/modules/packages.nix
@@ -4,6 +4,8 @@
     gopass
     gopass-jsonapi
     yt-dlp
+    gdu # fast disk usage analyzer with console interface written in go
+    duf # disk usage/free utility - a better 'df' alternative 
   ];
 
 }
