55ca3162b46f4fd507776d61351c0bb9663aad0c
Jacob Bachmann <jacob.bachmann@posteo.de>
2024-01-25 18:49:57 +0100

    fix: build man cache db

--- modules/man.nix
+++ modules/man.nix
@@ -4,7 +4,12 @@
     enable = true;
     doc.enable = true;
     dev.enable = true;
-    man.enable = true;
+    man = {
+      enable = true;
+      generateCaches = true;
+      man-db.enable = true;
+    };
+
   };
 
   environment.systemPackages = with pkgs; [
