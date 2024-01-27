6b3807de3993a30dffedfe10eda553e70a21ae7d
Jacob Bachmann <jacob.bachmann@posteo.de>
2024-01-27 23:03:11 +0100

    feat: add some utilities

--- users/gandalf/modules/packages.nix
+++ users/gandalf/modules/packages.nix
@@ -4,8 +4,11 @@
     gopass
     gopass-jsonapi
     yt-dlp
+    ranger # a vim-inspired filemanager for the console 
     gdu # fast disk usage analyzer with console interface written in go
-    duf # disk usage/free utility - a better 'df' alternative 
+    duf # disk usage/free utility - a better 'df' alternative
+    phoronix-test-suite # the phoronix test suite open-source, cross-platform automated testing/benchmarking software
+    ffmpeg
   ];
 
 }
