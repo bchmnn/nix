8fee4883b81c2eb87b4d105dc053553cd04f7d37
Jacob Bachmann <jacob.bachmann@posteo.de>
2024-01-27 23:02:54 +0100

    feat: add gaming profile

--- users/gandalf/modules/gui/sway/kanshi.nix
+++ users/gandalf/modules/gui/sway/kanshi.nix
@@ -14,6 +14,29 @@
         ];
       };
 
+      gaming = {
+        outputs = [
+          {
+            criteria = "LVDS-1";
+            status = "disable";
+          }
+          {
+            criteria = "Dell Inc. DELL U2515H 9X2VY5490XUL";
+            status = "disable";
+          }
+          {
+            criteria = "Dell Inc. DELL U2515H 9X2VY5C7138L";
+            status = "enable";
+            mode = "1920x1080";
+            position = "0,0";
+          }
+          {
+            criteria = "HJW VGA TO HDMI 0x00000100";
+            status = "disable";
+          }
+        ];
+      };
+
       station = {
         outputs = [
           {
