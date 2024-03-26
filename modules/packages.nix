608dd81dc16ffbc8268c7d26f17c88a3ee6586c1
Jacob Bachmann <jacob.bachmann@posteo.de>
2024-03-26 11:35:54 +0100

    feat: add unrar

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,22 +1,23 @@
 { lib, pkgs, config, ... }: {
   environment.systemPackages = with pkgs; with config.bchmnn; [
-    pciutils # A collection of programs for inspecting and manipulating configuration of PCI devices
-    usbutils # Tools for working with USB devices, such as lsusb
-    git # Distributed version control system
-    gnumake # A tool to control the generation of non-source files from sources
-    mercurial # A fast, lightweight SCM system for very large distributed projects
-    wget # Tool for retrieving files using HTTP, HTTPS, and FTP
-    neofetch # A fast, highly customizable system info script
-    lsd # The next gen ls command
-    ripgrep # A utility that combines the usability of The Silver Searcher with the raw speed of grep
-    ripgrep-all # Ripgrep, but also search in PDFs, E-Books, Office documents, zip, tar.gz, and more
-    expect # A tool for automating interactive applications
-    jq # A lightweight and flexible command-line JSON processor
-    unzip # An extraction utility for archives compressed in .zip format
-    libsecret # A library for storing and retrieving passwords and other secrets
-    xidel # Command line tool to download and extract data from HTML/XML pages as well as JSON APIs
-    nvd # Nix/NixOS package version diff tool
-    inotify-tools # a C library and a set of command-line programs providing a simple interface to inotify
+    pciutils # a collection of programs for inspecting and manipulating configuration of pci devices
+    usbutils # tools for working with usb devices, such as lsusb
+    git # distributed version control system
+    gnumake # a tool to control the generation of non-source files from sources
+    mercurial # a fast, lightweight scm system for very large distributed projects
+    wget # tool for retrieving files using http, https, and ftp
+    neofetch # a fast, highly customizable system info script
+    lsd # the next gen ls command
+    ripgrep # a utility that combines the usability of the silver searcher with the raw speed of grep
+    ripgrep-all # ripgrep, but also search in pdfs, e-books, office documents, zip, tar.gz, and more
+    expect # a tool for automating interactive applications
+    jq # a lightweight and flexible command-line json processor
+    unzip # an extraction utility for archives compressed in .zip format
+    unrar # utility for rar archives
+    libsecret # a library for storing and retrieving passwords and other secrets
+    xidel # command line tool to download and extract data from html/xml pages as well as json apis
+    nvd # nix/nixos package version diff tool
+    inotify-tools # a c library and a set of command-line programs providing a simple interface to inotify
     ghostscript # postscript interpreter (mainline version)
     ventoy-full # bootable usb solution
     fio # flexible io tester - an io benchmark tool
@@ -40,8 +41,8 @@
     nodePackages.pnpm
     mongodb-compass
   ] ++ lib.optionals (gui.enable) [
-    libnotify # A library that sends desktop notifications to a notification daemon
-    xdg-utils # A set of command line tools that assist applications with a variety of desktop integration tasks
+    libnotify # a library that sends desktop notifications to a notification daemon
+    xdg-utils # a set of command line tools that assist applications with a variety of desktop integration tasks
   ] ++ lib.optionals (gui.enable && lib.elem "sway" gui.flavour) [
     qt5.qtwayland
     qt6.qtwayland
