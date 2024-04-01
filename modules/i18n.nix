c47bef985a05419c2b11a75d637375c84ea7fbb8
Jacob Bachmann <jacob.bachmann@posteo.de>
2024-04-01 12:29:25 +0200

    feat: add de_DE locale

--- modules/i18n.nix
+++ modules/i18n.nix
@@ -3,7 +3,10 @@
   time.timeZone = "Europe/Berlin";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    supportedLocales = [ "en_US.UTF-8/UTF-8" "de_DE.UTF-8/UTF-8" ];
+  };
   console = {
     font = "Lat2-Terminus16";
     keyMap = "us";
