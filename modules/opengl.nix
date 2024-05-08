79b315302055ded496ca196b9911074806cf72fb
Jacob Bachmann <jacob.bachmann@posteo.de>
2024-05-08 23:35:38 +0200

    feat: use intel-media-driver if available

--- modules/opengl.nix
+++ modules/opengl.nix
@@ -1,7 +1,16 @@
-{ ... }: {
+{ config, lib, pkgs, ... }: {
   hardware.opengl = {
     enable = true;
     driSupport = true;
     driSupport32Bit = true;
   };
+
+  environment.variables = lib.mkIf (
+    builtins.elem
+    pkgs.intel-media-driver
+    config.hardware.opengl.extraPackages
+  ) {
+    LIBVA_DRIVER_NAME = "iHD";
+  };
+
 }
