f451acb559be028b5ed44d28e469ce3d2501ae05
Jacob Bachmann <jacob.bachmann@posteo.de>
2024-08-21 12:20:55 +0200

    feat: a lot of stuff

--- users/gandalf/modules/scripts/default.nix
+++ users/gandalf/modules/scripts/default.nix
@@ -1,6 +1,9 @@
 { pkgs, nixosConfig, ... }: {
+  imports = [
+    ./pomodoro.nix
+  ];
   home.packages = [
     (import ./git-clone-list.nix { inherit pkgs; })
     (import ./nix-update.nix { inherit pkgs nixosConfig; })
-  ] ++ (import ./pomodoro.nix { inherit pkgs; });
+  ];
 }
--- users/gandalf/modules/scripts/pomodoro.nix
+++ users/gandalf/modules/scripts/pomodoro.nix
@@ -1,20 +1,23 @@
-{ pkgs }:
+{ pkgs, ... }:
 let
   pootis = ./pootis.m4a;
 in
-[
-  (pkgs.writeShellScriptBin
-    "pomostart"
-    ''
-      clear
-      ${pkgs.openpomodoro-cli}/bin/openpomodoro-cli start --wait $@
-      while ${pkgs.mpv}/bin/mpv ${pootis} > /dev/null 2>&1; do :; done
-    '')
-  (pkgs.writeShellScriptBin
-    "pomobreak"
-    ''
-      clear
-      ${pkgs.openpomodoro-cli}/bin/openpomodoro-cli break --wait $@
-      while ${pkgs.mpv}/bin/mpv ${pootis} > /dev/null 2>&1; do :; done
-    '')
-]
+{
+  home.packages = [
+    pkgs.openpomodoro-cli
+    (pkgs.writeShellScriptBin
+      "pomostart"
+      ''
+        clear
+        ${pkgs.openpomodoro-cli}/bin/openpomodoro-cli start --wait $@
+        while ${pkgs.mpv}/bin/mpv ${pootis} > /dev/null 2>&1; do :; done
+      '')
+    (pkgs.writeShellScriptBin
+      "pomobreak"
+      ''
+        clear
+        ${pkgs.openpomodoro-cli}/bin/openpomodoro-cli break --wait $@
+        while ${pkgs.mpv}/bin/mpv ${pootis} > /dev/null 2>&1; do :; done
+      '')
+  ];
+}
